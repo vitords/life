092a19e89129e8eb01facb30d120d2cfa361f235
Vitor da Silva <vitords.42@gmail.com>
2021-11-23 01:52:03 +0100

    Count neighbors, evolution logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,85 @@
+use std::fmt;
+struct Life {
+    board: [[i32; 3]; 3],
+}
+
+impl fmt::Debug for Life {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.board.fmt(f)
+    }
+}
+
+fn neighbours(life: &mut Life, i: usize, j: usize) -> i32 {
+    // i-1,j-1,  i-1,j,  i-1,j+1
+    //   i,j-1,            i,j+1
+    // i+1,j-1,  i+1,j,  i+1,j+1
+    let mut total = 0;
+
+    if i > 0 {
+        if j > 0 {
+            total += life.board[i-1][j-1];
+        }
+        total += life.board[i-1][j];
+        if j < life.board[i].len() - 1{
+            total += life.board[i-1][j+1];
+        }
+    }
+    if j > 0 {
+        total += life.board[i][j-1];
+    }
+    if j < life.board[i].len() - 1 {
+        total += life.board[i][j+1];
+    }
+    if i < life.board.len() - 1 {
+        if j > 0 {
+            total += life.board[i+1][j-1];
+        }
+        total += life.board[i+1][j];
+        if j < life.board[i].len() - 1{
+            total += life.board[i+1][j+1];
+        }
+        
+    }
+    total
+}
+
+fn evolve(life: &mut Life) {
+    // 1. Any live cell with two or three live neighbours survives.
+    // 2. Any dead cell with three live neighbours becomes a live cell.
+    // 3. All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+    let mut new_board = life.board.clone();
+
+    for i in 0..life.board.len() {
+        for j in 0..life.board[i].len() {
+            let n = neighbours(life, i, j);
+
+            if (n == 2 || n == 3) && life.board[i][j] == 1 {
+                new_board[i][j] = 1;
+            } else if n == 3 && life.board[i][j] == 0 {
+                new_board[i][j] = 1;
+            } else {
+                new_board[i][j] = 0;
+            }
+        }
+    }
+    life.board = new_board;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut life = Life{board: [[0; 3]; 3]};
+    let generations = 3;
+
+    life.board[1][0] = 1;
+    life.board[1][1] = 1;
+    life.board[1][2] = 1;
+    println!("Seed:\n{:?}", life);
+
+    let mut gen = 0;
+    while gen < generations {
+        evolve(&mut life);
+        println!("Generation {}...", gen);
+        println!("{:?}", life);
+        gen += 1;
+    }
+
 }
